2de8ef90671ba3185394781b401ff888113dc461
harshvaland04 <harshkumar.valand@dcmail.ca>
2024-08-02 15:24:57 -0400

    class add

--- Assignment 4/Part-3/main.js
+++ Assignment 4/Part-3/main.js
@@ -12,8 +12,45 @@ function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random color
+// function to generate random RGB color value
 
 function randomRGB() {
-  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
-}
+    return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+  }
+  
+  class Ball {
+    constructor(x, y, velX, velY, color, size) {
+      this.x = x;
+      this.y = y;
+      this.velX = velX;
+      this.velY = velY;
+      this.color = color;
+      this.size = size;
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+      }
+    
+      update() {
+        if (this.x + this.size >= width) {
+          this.velX = -Math.abs(this.velX);
+        }
+    
+        if (this.x - this.size <= 0) {
+          this.velX = Math.abs(this.velX);
+        }
+    
+        if (this.y + this.size >= height) {
+          this.velY = -Math.abs(this.velY);
+        }
+    
+        if (this.y - this.size <= 0) {
+          this.velY = Math.abs(this.velY);
+        }
+    
+        this.x += this.velX;
+        this.y += this.velY;
+      }
